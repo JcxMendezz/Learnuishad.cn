bba2c5e8f33a8fd44998798dca0207c3642a2fd5
JcxMendezz <juanmendez17t@gmail.com>
2024-08-19 20:07:04 -0500

    refactor: Component Input-OTP

--- src/app/dashboard/Input-OTP/page.tsx
+++ src/app/dashboard/Input-OTP/page.tsx
@@ -4,29 +4,24 @@ import {
   InputOTPGroup,
   InputOTPSeparator,
   InputOTPSlot,
-} from "@/components/ui/input-otp"
-
+} from "@/components/ui/input-otp";
 
 export default function InputOTPDemo() {
   return (
-    <div className="flex w-full" >
+    <div className="flex w-full">
       <InputOTP maxLength={6}>
-      <InputOTPGroup className="  w-500 justify-center items-center">
-        <InputOTPSlot index={0} />
-        <InputOTPSlot index={1} />
-        <InputOTPSlot index={2} />
-      </InputOTPGroup>
-      <InputOTPSeparator />
-      <InputOTPGroup>
-        <InputOTPSlot index={3} />
-        <InputOTPSlot index={4} />
-        <InputOTPSlot index={5} />
-      </InputOTPGroup>
-    </InputOTP>
+        <InputOTPGroup className="  w-500 justify-center items-center">
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
     </div>
-    
-  )
+  );
 }
-
-
-  
